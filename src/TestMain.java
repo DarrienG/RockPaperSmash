c396e71cb4031bbf4ad693199aa43e32a904e961
Darrien Glasser <darrienglasser@outlook.com>
2015-12-20 07:15:54 -0500

    Added "intelligent" answer parsing.

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,12 +1,36 @@
 import java.util.Scanner;
 
 public class TestMain{
+
+    public static final String[] posResponses = {"y", "yes", "1", "yup", "good", "check", "y e s", "praise duarte", "hell yeah", "hell yes"};
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String p1Name, p2Name;
 
         while (true){
-            
+            p1Name = getName("Player 1");
+            p2Name = getName("Player 2");
+            println(p1Name);
+            println(p2Name);
+        }
+    }
+
+    public static String getName(String player){
+        String input, response;
+        Scanner sc = new Scanner(System.in);
+        println("");
+        while (true){
+            print(player + " please enter your tag: ");
+            input = sc.nextLine();
+            print("Is \"" + input + "\" good?\n> ");
+            response = sc.nextLine();
+            response = response.toLowerCase();
+            for (String comparator : posResponses){
+                if (response.equals(comparator)){
+                    return input;
+                }
+            }
         }
     }
 
