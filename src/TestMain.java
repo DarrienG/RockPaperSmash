9bd00db2f1046bb9611e711ba2a90bfca255fc7f
Darrien Glasser <darrienglasser@outlook.com>
2015-12-19 19:21:13 -0500

    Kind of modified main. Sync for later.

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,37 +1,20 @@
+import java.util.Scanner;
+
 public class TestMain{
     public static void main(String args[]){
-        // debugChar();
+        Scanner sc = new Scanner(System.in);
+        String p1Name, p2Name;
+
+        while (true){
+            
+        }
     }
 
+
+    // Makes printing easier and shorter
     public static void println(Object object){
         System.out.println(object);
     }
+    public static void print(Object object) { System.out.print(object); }
 
-    // For debugging characters based on stats
-
-    /*
-    public static void debugChar(){
-    Champion ch = new Champion("test1.txt");
-    for (int i = 0; i < ch.getFileNames().length; ++i){
-        println(ch.getFileNames()[i]);
-    }
-
-    System.out.println("Atk stats");
-    println(ch.getAtkDmg());
-    println(ch.getAtkKB());
-    println(ch.getAtkDir());
-
-    println("Grab stats");
-    println(ch.getGrabDmg());
-    println(ch.getGrabKB());
-    println(ch.getGrabDir());
-
-    println("Shield stats");
-    println(ch.getShieldDmg());
-    println(ch.getShieldKB());
-    println(ch.getShieldDir());
-
-    println("Weight: " + ch.getWeight());
-    }
-    */
 }
