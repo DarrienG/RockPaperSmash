934e12d83af1c5f20a2ffa02043bb672cb31e191
Darrien Glasser <darrienglasser@outlook.com>
2015-12-22 07:32:44 -0500

    Well it looks awful, but it works. Make it look pretty later.

--- src/Driver.java
+++ src/Driver.java
@@ -5,6 +5,10 @@ public class Driver {
 
     public static final String[] posResponses = {"y", "yes", "1", "yup", "good", "check", "yeah", "y e s", "praise duarte", "hell yeah", "hell yes"};
     public static final String[] negResponses = {"n", "no", "0", "nada", "bad", "x", "never", "n o", "praise ive", "hell no"};
+    public static final String[] attackOps = {"attack", "a", "1"};
+    public static final String[] grabOps = {"grab", "g", "2"};
+    public static final String[] shieldOps = {"shield", "s", "3"};
+
     public static final Champion[] charList = {new Marth("NULL")};
     public static final Stage[] stageList = {new Battlefield()};
 
@@ -72,7 +76,6 @@ public class Driver {
                 println("");
             }
 
-
             // Only go here if the player chose a valid Champion
             if (goodPick){
                 print("Is \"" + input + "\" good?\n> ");
@@ -124,8 +127,7 @@ public class Driver {
                 println("");
             }
 
-
-            // Only go here if the player chose a valid Champion
+            // Only go here if the player chose a valid Stage
             if (goodPick) {
                 print("Is \"" + input + "\" good?\n> ");
                 response = sc.nextLine();
@@ -168,53 +170,118 @@ public class Driver {
 
     public static void battleBegin(Champion first, Champion second, Stage arena){
         Scanner sc = new Scanner(System.in);
-        String input;
+        String inputFirst, inputSecond;
+        double knockBack;
         while(!first.isKO() && !second.isKO()){
-            println(first.getCharName() + ", what move do you want to make? a, g, s\n> ");
-            input = sc.nextLine();
+            println(first.getCharName() + "\t:\t" + first.getChampionName() + "\t:\t%" + first.getPercentDmg());
+            println(second.getCharName() + "\t:\t" + second.getChampionName() + "\t:\t%" + second.getPercentDmg());
+
+            print(first.getCharName() + ", what move do you want to make? a, g, s\n> ");
+            inputFirst = sc.nextLine();
+
+
+            print(second.getCharName() + ", what move do you want to make? a, g, s\n> ");
+            inputSecond = sc.nextLine();
             // Do the array thing with choices tomorrow
-            if (input.equals("a")){
-                first.setActionFlag(0);
+
+            first.setActionFlag(-1); second.setActionFlag(-1);
+            for (String a : attackOps){
+                if (inputFirst.toLowerCase().trim().equals(a)){
+                    first.setActionFlag(0);
+                }
+                if (inputSecond.toLowerCase().trim().equals(a)){
+                    second.setActionFlag(0);
+                }
             }
-            if (input.equals("s")){
-                first.setActionFlag(1);
+            for (String g: grabOps){
+                if (inputFirst.toLowerCase().trim().equals(g)){
+                    first.setActionFlag(1);
+                }
+                if (inputSecond.toLowerCase().trim().equals(g)){
+                    second.setActionFlag(1);
+                }
             }
+            for (String s: shieldOps){
+                if (inputFirst.toLowerCase().trim().equals(s)){
+                    first.setActionFlag(2);
+                }
+                if (inputSecond.toLowerCase().trim().equals(s)){
+                    second.setActionFlag(2);
+                }
+            }
+
+            if (first.getActionFlag() != -1 && second.getActionFlag() != -1){
+                // Print result of each attack
+                // As a reminder, 0 = attack (rock), 1 = grab (scissors), 2 = shield (paper)
+
+                // TODO: Get knockback for each, use flag to determine who was hit, see if opposing player dies
+                int p1Action = first.getActionFlag(), p2Action = second.getActionFlag();
+                if (p1Action == 0 && p2Action == 0 || p1Action == 1 && p2Action == 1 || p1Action == 2 && p2Action == 2){
+                    println("Same option chosen by " + first.getCharName() + " and " + second.getCharName() + "!\nNo damage taken.");
+                }
+
+                if (p2Action == p1Action + 1 || p2Action == p1Action - 2){
+                    knockBack = first.attack(second);
+
+                    // Is knockback horizontal?
+                    if (first.getStats()[first.getActionFlag()][2] == 0){
+                        knockBack = (second.getPercentDmg() * knockBack) * second.getGravity();
+
+                        if (knockBack > arena.getHorizontalLen()){
+                            second.toggleKO();
+                            println(second.getCharName() + " has been KO'd!");
+                        }
+                    }
+
+                    // Or is is vertical?
+                    else{
+                        knockBack = (second.getPercentDmg() * knockBack) / second.getGravity();
+
+                        if (knockBack > arena.getVerticalLen()){
+                            second.toggleKO();
+                            println(second.getCharName() + " has been KO'd!");
+                        }
+                    }
+
+
+                }else{
+                    knockBack = second.attack(first);
+                    // Is knockback horizontal
+                    if (second.getStats()[second.getActionFlag()][2] == 0){
+                        knockBack = (first.getPercentDmg() * knockBack) * first.getGravity();
+
+                        if (knockBack > arena.getHorizontalLen()){
+                            first.toggleKO();
+                            println(first.getCharName() + " has been KO'd!");
+                        }
+                    }
+
+                    // Or is is vertical?
+                    else{
+                        knockBack = (first.getPercentDmg() * knockBack) / first.getGravity();
+
+                        if (knockBack > arena.getVerticalLen()){
+                            first.toggleKO();
+                            println(first.getCharName() + " has been KO'd!");
+                        }
+                    }
+
+                }
+
+                knockBack = 0;
+
+            }
+
+            // Invalid action entered by one of the two players
             else{
-                first.setActionFlag(2);
+                String misMatch = first.getActionFlag() == -1 ? first.getCharName() : second.getCharName();
+                println("Invalid action entered by " + misMatch + " please re-enter commands.");
             }
+            // TODO: End battle, possible increment counter, champs may require extra data member to determine player number
 
-            println(second.getCharName() + ", what move do you want to make? a, g, s\n> ");
-            input = sc.nextLine();
-            // Do the array thing with choices tomorrow
-            if (input.equals("a")){
-                second.setActionFlag(0);
-            }
-            if (input.equals("s")){
-                second.setActionFlag(1);
-            }
-            else{
-                second.setActionFlag(2);
-            }
-
-            // Print result of each attack
-            // As a reminder, 0 = attack (rock), 1 = grab (scissors), 2 = shield (paper)
-
-            // TODO: Get knockback for each, use flag to determine who was hit, see if opposing player dies
-            int p1Action = first.getActionFlag(), p2Action = second.getActionFlag();
-            if (p1Action == 0 && p2Action == 0 || p1Action == 1 && p2Action == 1 || p1Action == 2 && p2Action == 2){
-                // print out draw
-            }
-
-            if (p2Action == p1Action + 1 || p2Action == p1Action - 2){
-                first.attack(second);
-            }else{
-                second.attack(first);
-            }
-
-            // TODO: If players survive, print out percentages, do it at the top of the loop though, not here
         }
 
-        // TODO: End battle, possible increment counter, champs may require extra data member to detemine player number
+
     }
 
     // Copies values from one champion into a temporary Champion to later be returned
@@ -223,12 +290,13 @@ public class Driver {
     // SO MUCH DUPLICATE CODE
     // AAAAAAAAAAAAAAAAAAHHUFGHSDPGFUDSILFGBUP:GBPGFUI
     public static Champion clone(Champion ch){
-        if (ch.getChampionName().toLowerCase() == "marth"){
+        if (ch.getChampionName().toLowerCase().equals("marth")){
             Champion tmpChamp = new Marth(ch.getCharName());
             return tmpChamp;
         }
 
         // Default to Marth if there is some impossible logic flaw that allows this to slip through the cracks
+        println("Impossible logic flaw. Defaulting to Marth.");
         Champion tmpChamp = new Marth(ch.getCharName());
         return tmpChamp;
     }
